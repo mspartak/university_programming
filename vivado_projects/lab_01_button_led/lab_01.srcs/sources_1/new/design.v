b7f1b9317e22720479be4e6c1b3328fe4445897f
mspartak <mspartak7@gmail.com>
2025-09-14 21:52:17 +0300

    initial project (no tb, no functionality)

---
+++ vivado_projects/lab_01_button_led/lab_01.srcs/sources_1/new/design.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/14/2025 09:41:58 PM
+// Design Name: 
+// Module Name: design
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module design(
+    input x1,
+    input x2,
+    output y1,
+    output y2,
+    output y3,
+    output y4
+    );
+    
+    reg [3:0] dummy = 0;
+    
+    assign y1 = dummy[0];
+    assign y2 = dummy[1];
+    assign y3 = dummy[2];
+    assign y4 = dummy[3];
+    
+endmodule
